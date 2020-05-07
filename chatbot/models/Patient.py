5e34904edc9144e2e4976c177ce731073eb7cbb7
Carlos Sánchez <karlossanpa@gmail.com>
2020-05-07 19:28:02 +0200

    Modify .gitignore

---
+++ chatbot/models/Patient.py
@@ -0,0 +1,7 @@
+
+class Patient(object):
+    def __init__(self, name="", picture="", country="", gender = ""):
+        self.name = name
+        self.picture = picture
+        self.country = country
+        self.gender = gender
