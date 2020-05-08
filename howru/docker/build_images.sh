9e0227e338d4cff3748966ca4199907d916157b5
Carlos Sánchez <karlossanpa@gmail.com>
2020-05-08 17:39:31 +0200

    Add web_interface container

--- howru/docker/build_images.sh
+++ howru/docker/build_images.sh
@@ -2,5 +2,7 @@
 # Chatbot
 docker build -t psychologist-bot chatbot
 docker build -t mongo-populator mongo-populator
+docker build -t nginx nginx
 
 # Web interface
+docker build -t web_interface web_interface
