cb17ddd36958e230111c4a8a644c4139bbf42f2a
Carlos Sánchez <karlossanpa@gmail.com>
2020-05-09 17:07:54 +0200

    Update views

--- howru/web_interface/patients_manager/views.py
+++ howru/web_interface/patients_manager/views.py
@@ -6,16 +6,16 @@ def assign(request):
     context = {
         'test_var': 'TEST VARIABLE THAT COMES FROM PYTHON CODE (PATIENTS MANAGER/ASSIGN)',
     }
-    return render(request, 'patients_manager/index.html', context)
+    return render(request, 'patients_manager/assign.html', context)
 
 def unassign(request):
     context = {
         'test_var': 'TEST VARIABLE THAT COMES FROM PYTHON CODE (PATIENTS MANAGER/UNASSIGN)',
     }
-    return render(request, 'patients_manager/index.html', context)
+    return render(request, 'patients_manager/unassign.html', context)
 
 def view(request):
     context = {
         'test_var': 'TEST VARIABLE THAT COMES FROM PYTHON CODE (PATIENTS MANAGER/VIEW)',
     }
-    return render(request, 'patients_manager/index.html', context)
+    return render(request, 'patients_manager/view.html', context)
--- howru/web_interface/questions_manager/views.py
+++ howru/web_interface/questions_manager/views.py
@@ -6,22 +6,22 @@ def create(request):
     context = {
         'test_var': 'TEST VARIABLE THAT COMES FROM PYTHON CODE (QUESTIONS MANAGER/CREATE)',
     }
-    return render(request, 'questions_manager/index.html', context)
+    return render(request, 'questions_manager/create.html', context)
 
 def modify(request):
     context = {
         'test_var': 'TEST VARIABLE THAT COMES FROM PYTHON CODE (QUESTIONS MANAGER/MODIFY)',
     }
-    return render(request, 'questions_manager/index.html', context)
+    return render(request, 'questions_manager/modify.html', context)
 
 def delete(request):
     context = {
         'test_var': 'TEST VARIABLE THAT COMES FROM PYTHON CODE (QUESTIONS MANAGER/DELETE)',
     }
-    return render(request, 'questions_manager/index.html', context)
+    return render(request, 'questions_manager/delete.html', context)
 
 def view(request):
     context = {
         'test_var': 'TEST VARIABLE THAT COMES FROM PYTHON CODE (QUESTIONS MANAGER/VIEW)',
     }
-    return render(request, 'questions_manager/index.html', context)
+    return render(request, 'questions_manager/view.html', context)
