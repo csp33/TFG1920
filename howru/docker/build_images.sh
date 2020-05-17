7fe173c5bda6071aad91fc2d60c4c7628550ed5a
Carlos Sánchez <karlossanpa@gmail.com>
2020-05-17 18:18:00 +0200

    Migrate to PostgreSQL

--- howru/docker/build_images.sh
+++ howru/docker/build_images.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
+
 # Chatbot
 docker build -t psychologist-bot chatbot
-docker build -t mongo-populator mongo-populator
-docker build -t nginx nginx
 
 # Web interface
 docker build -t web_interface web_interface
+docker build -t nginx nginx
